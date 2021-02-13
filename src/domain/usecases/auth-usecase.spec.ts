5863a45bcc2333d188d602460e8d964b448ea809
Daniel Lucas <daniellucas382@gmail.com>
2021-02-13 16:27:37 -0300

    ensure AuthUseCase calls LoadUserRepository with correct email

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,22 +1,36 @@
+import ILoadUserRepository from '@/infra/repositories/load-user-repository-interface'
 import { MissingParamError } from '@/utils/errors'
 
 class AuthUseCase {
-  async auth (email: string, password: string): Promise<string> {
-    if (!email) {
-      throw new MissingParamError('email')
-    }
+  constructor (private readonly loadUserRepository: ILoadUserRepository) { }
 
-    if (!password) {
-      throw new MissingParamError('password')
-    }
+  async auth (email: string, password: string): Promise<string> {
+    if (!email) { throw new MissingParamError('email') }
+    if (!password) { throw new MissingParamError('password') }
+
+    await this.loadUserRepository.load(email)
 
     return ''
   }
 }
 
 function makeSut (): any {
-  const sut = new AuthUseCase()
-  return { sut }
+  const loadUserRepositorySpy = makeLoadUserRepositorySpy()
+  const sut = new AuthUseCase(loadUserRepositorySpy)
+  return { sut, loadUserRepositorySpy }
+}
+
+function makeLoadUserRepositorySpy (): any {
+  class LoadUserRepositorySpy {
+    email: string = ''
+
+    async load (email: string): Promise<any> {
+      this.email = email
+      return {}
+    }
+  }
+
+  return new LoadUserRepositorySpy()
 }
 
 describe('Auth UseCase', function () {
@@ -32,5 +46,11 @@ describe('Auth UseCase', function () {
     const promise = sut.auth('any@email.com', null)
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  it('should call LoadUserRepository with correct email', async function () {
+    const { sut, loadUserRepositorySpy } = makeSut()
+    await sut.auth('any@email.com', 'any_password')
+    expect(loadUserRepositorySpy.email).toBe('any@email.com')
   })
 })
