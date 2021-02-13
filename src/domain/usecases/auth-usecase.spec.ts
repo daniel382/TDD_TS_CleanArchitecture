c8ad2477a38a2ae0de006c979325931f620de9ca
Daniel Lucas <daniellucas382@gmail.com>
2021-02-13 16:47:24 -0300

    moving AuthUseCase to its own file

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,23 +1,6 @@
 import ILoadUserRepository from '@/infra/repositories/load-user-repository-interface'
 import { InvalidParamError, MissingParamError } from '@/utils/errors'
-
-class AuthUseCase {
-  constructor (private readonly loadUserRepository: ILoadUserRepository) { }
-
-  async auth (email: string, password: string): Promise<string | null> {
-    if (!email) { throw new MissingParamError('email') }
-    if (!password) { throw new MissingParamError('password') }
-    if (!this.loadUserRepository) { throw new MissingParamError('loadUserRepository') }
-    if (!this.loadUserRepository.load) { throw new InvalidParamError('loadUserRepository') }
-
-    const user = await this.loadUserRepository.load(email)
-    if (!user) {
-      return null
-    }
-
-    return ''
-  }
-}
+import AuthUseCase from './auth-usecase'
 
 function makeSut (): any {
   const loadUserRepositorySpy = makeLoadUserRepositorySpy()
---
+++ src/domain/usecases/auth-usecase.ts
@@ -0,0 +1,22 @@
+import ILoadUserRepository from '@/infra/repositories/load-user-repository-interface'
+import { InvalidParamError, MissingParamError } from '@/utils/errors'
+
+class AuthUseCase {
+  constructor (private readonly loadUserRepository: ILoadUserRepository) { }
+
+  async auth (email: string, password: string): Promise<string | null> {
+    if (!email) { throw new MissingParamError('email') }
+    if (!password) { throw new MissingParamError('password') }
+    if (!this.loadUserRepository) { throw new MissingParamError('loadUserRepository') }
+    if (!this.loadUserRepository.load) { throw new InvalidParamError('loadUserRepository') }
+
+    const user = await this.loadUserRepository.load(email)
+    if (!user) {
+      return null
+    }
+
+    return ''
+  }
+}
+
+export default AuthUseCase
